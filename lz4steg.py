dcba06216db2dc9e042bbbe971a8f36113b889c0
Jan Varho <jan@varho.org>
2014-04-18 11:24:12 +0300

    Refactor common LZ77 steganography for eventual Snappy support

--- lz4steg.py
+++ lz4steg.py
@@ -1,33 +1,9 @@
 #!/usr/bin/python
 
 import argparse
-import math
 import sys
 
-
-def _hash(b):
-    a = (b[0] << 24) + (b[1] << 16) + (b[2] << 8) + b[3]
-    a += ~(a<<15);
-    a ^=  (a>>10);
-    a +=  (a<<3);
-    a ^=  (a>>6);
-    a += ~(a<<11);
-    a ^=  (a>>16);
-    return a
-
-
-def _put_byte(window, table, chain, pos, b):
-    bytes = [
-        window[(pos-3) & 0xffff],
-        window[(pos-2) & 0xffff],
-        window[(pos-1) & 0xffff],
-        b
-    ]
-    window[pos & 0xffff] = b
-    h = _hash(bytes) & 0xffff
-    prev = table[h]
-    table[h] = pos-3
-    chain[(pos-3) & 0xffff] = prev
+from lz77steg import LZ77Steg, _hash
 
 
 def _match(window, pos, bytes):
@@ -40,252 +16,105 @@ def _match(window, pos, bytes):
     return True
 
 
-def _matches(window, table, chain, pos, mlen, moff):
-    assert mlen >= 4
-    if moff < mlen:
-        return [moff] # Too much trouble
-    mpos = pos - moff
-    match = [window[(mpos+i) & 0xffff] for i in range(mlen)]
-    bytes = match[:4]
-    h = _hash(bytes) & 0xffff
-    mlist = []
-    p = table[h]
-    while p is not None and p >= pos - 0xffff:
-        if _match(window, p, match):
-            mlist.append(pos - p)
-        p = chain[p & 0xffff]
-    if moff not in mlist:
-        mlist.append(moff)
-    return mlist
-
-
-def scan_lz4(cover, skip_bytes=4):
-    '''Scans an LZ4 string, finding how many bytes can be stored'''
-    randbits = 0
-    randpartbits = 0
-    cover = bytearray(cover)
-    window = bytearray(2**16)
-    table = [None] * (2**16)
-    chain = [None] * (2**16)
-    pos = 0
-    cpos = skip_bytes
-    cend = len(cover)
-    next_log = cend // 100
-    while cpos < cend:
-        if cpos > next_log:
-            print(
-                cpos * 100 // cend,
-                randbits // 8 * 100. / cpos,
-                randpartbits // 8 * 100. / cpos,
-            )
-            next_log += cend // 100
-        token = cover[cpos]
-        cpos += 1
-        llen = token >> 4
-        mlen = token & 0xf
-        
-        if llen == 15:
-            while cover[cpos] == 255:
-                llen += 255
-                cpos += 1
-            llen += cover[cpos]
-            cpos += 1
-        
-        while llen:
-            _put_byte(window, table, chain, pos, cover[cpos])
-            cpos += 1
-            pos += 1
-            llen -= 1
-        
-        if cpos == cend:
-            break
-        
-        moff = cover[cpos] + (cover[cpos+1] << 8)
-        cpos += 2
-        
-        if mlen == 15:
-            while cover[cpos] == 255:
-                mlen += 255
-                cpos += 1
-            mlen += cover[cpos]
-            cpos += 1
-        mlen += 4
-        
-        mlist = _matches(window, table, chain, pos, mlen, moff)
-        assert len(mlist)
-        
-        if len(mlist) > 1:
-            randbits += int(math.log(len(mlist), 2))
-            randpartbits += math.log(len(mlist), 2)
-        
-        while mlen:
-            _put_byte(window, table, chain, pos, window[(pos - moff) & 0xffff])
-            pos += 1
-            mlen -= 1
-    return randbits // 8, randpartbits // 8
-
-
-def hide_lz4(cover, message, skip_bytes=4, store_len=True):
-    '''Stores the message in an LZ4 string, raises IOError if out of space'''
-    randbits = 0
-    randpartbits = 0
-    cover = bytearray(cover)
-    window = bytearray(2**16)
-    table = [None] * (2**16)
-    chain = [None] * (2**16)
-    pos = 0
-    cpos = skip_bytes
-    cend = len(cover)
+class LZ4Steg(LZ77Steg):
     
-    message = [ord(i) for i in message]
-    if store_len:
-        assert 0 < len(message) < 0xffff
-        message = [len(message) & 0xff, (len(message) >> 8) & 0xff] + message
-    msg_pos, msg_bit = 0, 0
-    while cpos < cend:
-        token = cover[cpos]
-        cpos += 1
-        llen = token >> 4
-        mlen = token & 0xf
-        
-        if llen == 15:
-            while cover[cpos] == 255:
-                llen += 255
-                cpos += 1
-            llen += cover[cpos]
-            cpos += 1
-        
-        while llen:
-            _put_byte(window, table, chain, pos, cover[cpos])
-            cpos += 1
-            pos += 1
-            llen -= 1
-        
-        if cpos == cend:
-            break
-        
-        moff_pos = cpos
-        moff = cover[cpos] + (cover[cpos+1] << 8)
-        cpos += 2
-        
-        if mlen == 15:
-            while cover[cpos] == 255:
-                mlen += 255
-                cpos += 1
-            mlen += cover[cpos]
-            cpos += 1
-        mlen += 4
-        
-        mlist = _matches(window, table, chain, pos, mlen, moff)
-        assert len(mlist)
-        
-        if len(mlist) > 1:
-            bits = int(math.log(len(mlist), 2))
-            randbits += bits
-            which = 0
-            for i in range(bits):
-                bit = (message[msg_pos] >> msg_bit) & 1
-                which += (bit << i)
-                msg_bit += 1
-                if msg_bit == 8:
-                    msg_bit = 0
-                    msg_pos += 1
-                    if msg_pos == len(message):
-                        break
-            cover[moff_pos] = mlist[which] & 0xff
-            cover[moff_pos + 1] = mlist[which] >> 8
-            if msg_pos == len(message):
-                break
-        
-        while mlen:
-            _put_byte(window, table, chain, pos, window[(pos - moff) & 0xffff])
-            pos += 1
-            mlen -= 1
-    if msg_pos < len(message):
-        raise IOError
-    return cover
-
-
-def unhide_lz4(cover, skip_bytes=4, read_len=True):
-    '''Reads the message from an LZ4 string'''
-    randbits = 0
-    randpartbits = 0
-    cover = bytearray(cover)
-    window = bytearray(2**16)
-    table = [None] * (2**16)
-    chain = [None] * (2**16)
-    pos = 0
-    cpos = skip_bytes
-    cend = len(cover)
+    TOK_LITERAL = 1
+    TOK_MATCH = 2
     
-    message = [0]
-    msg_pos, msg_bit = 0, 0
-    msg_len = None
-    while cpos < cend:
-        token = cover[cpos]
-        cpos += 1
-        llen = token >> 4
-        mlen = token & 0xf
-        
-        if llen == 15:
-            while cover[cpos] == 255:
-                llen += 255
-                cpos += 1
-            llen += cover[cpos]
-            cpos += 1
-        
-        while llen:
-            _put_byte(window, table, chain, pos, cover[cpos])
-            cpos += 1
-            pos += 1
-            llen -= 1
-        
-        if cpos == cend:
-            break
-        
-        moff_pos = cpos
-        moff = cover[cpos] + (cover[cpos+1] << 8)
-        cpos += 2
-        
-        if mlen == 15:
-            while cover[cpos] == 255:
-                mlen += 255
-                cpos += 1
-            mlen += cover[cpos]
-            cpos += 1
-        mlen += 4
-        
-        mlist = _matches(window, table, chain, pos, mlen, moff)
-        assert len(mlist)
-        
-        if len(mlist) > 1:
-            bits = int(math.log(len(mlist), 2))
-            randbits += bits
-            which = mlist.index(moff)
-            for i in range(bits):
-                bit = (which >> i) & 1
-                message[msg_pos] += bit << msg_bit
-                msg_bit += 1
-                if msg_bit == 8:
-                    msg_bit = 0
-                    msg_pos += 1
-                    if read_len and msg_pos == 2:
-                        msg_len = message[0] + (message[1] << 8)
-                    if msg_len and msg_pos == msg_len + 2:
-                        break
-                    message.append(0)
-            cover[moff_pos] = mlist[which] & 0xff
-            cover[moff_pos + 1] = mlist[which] >> 8
-            if msg_len and msg_pos == msg_len + 2:
-                break
-        
-        while mlen:
-            _put_byte(window, table, chain, pos, window[(pos - moff) & 0xffff])
-            pos += 1
-            mlen -= 1
-    if read_len:
-        message = message[2:]
-    return ''.join([chr(i) for i in message])
+    def get_tokens(self):
+        '''Generator for tokens, must be implemented'''
+        while self.cpos < self.cend:
+            token = self.cover[self.cpos]
+            self.cpos += 1
+            llen = token >> 4
+            mlen = token & 0xf
+            
+            if llen == 15:
+                while self.cover[self.cpos] == 255:
+                    llen += 255
+                    self.cpos += 1
+                llen += self.cover[self.cpos]
+                self.cpos += 1
+            
+            if llen:
+                yield (self.TOK_LITERAL, llen)
+            
+            if self.cpos == self.cend:
+                return
+            
+            opos = self.cpos
+            
+            moff = self.cover[self.cpos] + (self.cover[self.cpos+1] << 8)
+            self.cpos += 2
+            
+            if mlen == 15:
+                while self.cover[self.cpos] == 255:
+                    mlen += 255
+                    self.cpos += 1
+                mlen += self.cover[self.cpos]
+                self.cpos += 1
+            mlen += 4
+            
+            yield (self.TOK_MATCH, mlen, moff, opos)
+    
+    def is_match(self, t):
+        '''Is token a match token?'''
+        return t[0] == self.TOK_MATCH
+    
+    def update_window(self, t):
+        '''Update window with token'''
+        if t[0] == self.TOK_LITERAL:
+            self.update_window_literal(t[1])
+        elif t[0] == self.TOK_MATCH:
+            self.update_window_match(t[1], t[2])
+        else:
+            raise TypeError
+    
+    def scan(self, cover, skip=4):
+        '''Scans cover for capacity'''
+        self.init(cover)
+        self.cpos += skip
+        return super(LZ4Steg, self).scan()
+    
+    def list_possible_matches(self, t):
+        '''Return a list of possible matches for t'''
+        tt, mlen, moff, opos = t
+        if moff < mlen:
+            return [moff] # Too much trouble
+        mpos = self.pos - moff
+        match = [self.window[(mpos+i) & 0xffff] for i in range(mlen)]
+        bytes = match[:4]
+        h = _hash(bytes) & 0xffff
+        mlist = []
+        p = self.table[h]
+        while p is not None and p >= self.pos - 0xffff:
+            if _match(self.window, p, match):
+                mlist.append(self.pos - p)
+            p = self.chain[p & 0xffff]
+        if moff not in mlist:
+            mlist.append(moff)
+        return mlist
+    
+    def update_match(self, t, nmatch):
+        '''Updates cover token to new match, must be implemented'''
+        self.cover[t[3]] = nmatch & 0xff
+        self.cover[t[3] + 1] = nmatch >> 8
+    
+    def store(self, cover, message, skip=4):
+        '''Stores message in cover, returning the modified'''
+        self.init(cover)
+        self.cpos += skip
+        return super(LZ4Steg, self).store(message, nullterm=True)
+    
+    def get_index(self, mlist, t):
+        '''Get the index of the match'''
+        return mlist.index(t[2])
+    
+    def retrieve(self, cover, skip=4):
+        '''Retrieves message from cover'''
+        self.init(cover)
+        self.cpos += skip
+        return super(LZ4Steg, self).retrieve(nullterm=True)
 
 
 if __name__ == '__main__':
@@ -300,15 +129,15 @@ if __name__ == '__main__':
         cover = f.read()
     
     if args.decode:
-        message = unhide_lz4(cover)
+        message = LZ4Steg().retrieve(cover)
         print message
     elif args.message:
         assert len(args.message)
-        cover = hide_lz4(cover, args.message)
+        cover = LZ4Steg().store(cover, args.message)
         sys.stdout.write(cover)
     else:
-        bytes, pbytes = scan_lz4(cover)
-        print '%d (%d) bytes of storage in %d (%.2f %%)' % (
-            bytes, pbytes, len(cover), bytes * 100. / len(cover)
+        bytes = LZ4Steg().scan(cover)
+        print '%d bytes of storage in %d (%.2f %%)' % (
+            bytes, len(cover), bytes * 100. / len(cover)
         )
 
---
+++ lz77steg.py
@@ -0,0 +1,162 @@
+
+import math
+
+
+def _hash(b):
+    a = (b[0] << 24) + (b[1] << 16) + (b[2] << 8) + b[3]
+    a += ~(a<<15);
+    a ^=  (a>>10);
+    a +=  (a<<3);
+    a ^=  (a>>6);
+    a += ~(a<<11);
+    a ^=  (a>>16);
+    return a
+
+
+class LZ77Steg(object):
+    
+    def init(self, cover):
+        '''Must be called prior to scan/store/retrieve'''
+        self.cover = bytearray(cover)
+        self.window = bytearray(2**16)
+        self.table = [None] * (2**16)
+        self.chain = [None] * (2**16)
+        self.pos = self.cpos = 0
+        self.cend = len(self.cover)
+    
+    def get_tokens(self):
+        '''Generator for tokens, must be implemented'''
+        raise NotImplementedError
+    
+    def is_match(self, t):
+        '''Is token a match token?'''
+        raise NotImplementedError
+    
+    def put_byte(self, b):
+        bytes = [
+            self.window[(self.pos-3) & 0xffff],
+            self.window[(self.pos-2) & 0xffff],
+            self.window[(self.pos-1) & 0xffff],
+            b
+        ]
+        self.window[self.pos & 0xffff] = b
+        h = _hash(bytes) & 0xffff
+        prev = self.table[h]
+        self.table[h] = self.pos-3
+        self.chain[(self.pos-3) & 0xffff] = prev
+        self.pos += 1
+    
+    def update_window_literal(self, llen):
+        while llen:
+            self.put_byte(self.cover[self.cpos])
+            self.cpos += 1
+            llen -= 1
+    
+    def update_window_match(self, mlen, moffset):
+        while mlen:
+            self.put_byte(self.window[(self.pos - moffset) & 0xffff])
+            mlen -= 1
+    
+    def update_window(self, t):
+        '''Update window with token, can use the above two'''
+        raise NotImplementedError
+    
+    def scan(self):
+        '''Scans cover for capacity'''
+        capacity = 0
+        for t in self.get_tokens():
+            if self.is_match(t):
+                mlist = self.list_possible_matches(t)
+                bits = int(math.log(len(mlist), 2))
+                capacity += bits
+            self.update_window(t)
+        return capacity // 8
+
+    def get_message_bits(self, bits):
+        index = 0
+        for i in range(bits):
+            bit = (self.message[self.mpos] >> self.mbit) & 1
+            index += (bit << i)
+            self.mbit += 1
+            if self.mbit == 8:
+                self.mbit = 0
+                self.mpos += 1
+                if self.mpos == self.mlen:
+                    return index, True
+        return index, False
+    
+    def list_possible_matches(self, t):
+        '''Return a list of possible matches for t'''
+        raise NotImplementedError
+    
+    def update_match(self, t, nmatch):
+        '''Updates cover token to new match, must be implemented'''
+        raise NotImplementedError
+    
+    def store(self, message, storelen=False, nullterm=False):
+        '''Stores message in cover, returning the modified'''
+        self.message = [ord(i) for i in message]
+        if storelen and len(message) <= 0xffff:
+            ol = len(message)
+            self.message = [ol & 0xff, ol >> 8] + self.message
+        if nullterm:
+            self.message.append(0)
+        self.mpos = 0
+        self.mbit = 0
+        self.mlen = len(self.message)
+        for t in self.get_tokens():
+            if self.is_match(t):
+                mlist = self.list_possible_matches(t)
+                bits = int(math.log(len(mlist), 2))
+                index, exit = self.get_message_bits(bits)
+                nmatch = mlist[index]
+                self.update_match(t, nmatch)
+                if exit:
+                    break
+            self.update_window(t)
+        return self.cover
+    
+    def set_message_bits(self, bits, index):
+        for i in range(bits):
+            bit = (index >> i) & 1
+            self.message[self.mpos] += bit << self.mbit
+            self.mbit += 1
+            if self.mbit == 8:
+                self.mbit = 0
+                self.mpos += 1
+                if self.mpos == self.mlen:
+                    return True
+                self.message.append(0)
+        return False
+    
+    def get_index(self, mlist, t):
+        '''Get the index of the match'''
+        raise NotImplementedError
+    
+    def retrieve(self, retrievelen=False, nullterm=False):
+        '''Retrieves message from cover'''
+        self.message = [0]
+        self.mpos = 0
+        self.mbit = 0
+        self.mlen = -1
+        for t in self.get_tokens():
+            if self.is_match(t):
+                mlist = self.list_possible_matches(t)
+                bits = int(math.log(len(mlist), 2))
+                ompos = self.mpos
+                if self.set_message_bits(bits, self.get_index(mlist, t)):
+                    break
+                if nullterm and self.mpos > ompos:
+                    if 0 in self.message[ompos:self.mpos]:
+                        break
+                if retrievelen and self.mpos == 2:
+                    self.mlen = 2 + self.message[0] + (self.message[1] << 8)
+            self.update_window(t)
+        if retrievelen:
+            self.message = self.message[2:]
+        if nullterm:
+            while self.message[-1] == 0:
+                self.message = self.message[:-1]
+        return ''.join([chr(i) for i in self.message])
+
+
